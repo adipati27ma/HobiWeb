0575c8bec56990692a17363c7444deed485bcfcc
adipati27ma <adipati.cs.samsung@gmail.com>
2020-12-19 10:59:24 +0700

    fix bug on login.php, add quotes on []

--- file_admin/login.php
+++ file_admin/login.php
@@ -2,7 +2,7 @@
 ob_start();
 include "../koneksi.php"; 
 $username = $_POST['username']; 
-$pass     = md5($_POST['password']);
+$pass     = ($_POST['password']);
 
 $login 	= mysqli_query( $link, "SELECT * FROM admin WHERE username='$username' AND password='$pass'"); 
 $ketemu = mysqli_num_rows($login); 
@@ -10,15 +10,15 @@ $r 		= mysqli_fetch_array($login);
 
 if ($ketemu > 0) { 
 
-    $_SESSION[username] = $r[username]; 
+    $_SESSION["username"] = $r["username"]; 
 
-    $_SESSION[password] = $r[password]; 
+    $_SESSION["password"] = $r["password"]; 
 
     //$_SESSION[level]	= $r[level]; --aktifkan jika ingin memakai level admin
 header('location:admin');
 }else{
 ?><script>alert("Login gagal!");document.location.href="loginadmin?pesan=gagal"</script>
 <?php
-echo mysqli_error();
+echo mysqli_error($error);
 }
 ?>
